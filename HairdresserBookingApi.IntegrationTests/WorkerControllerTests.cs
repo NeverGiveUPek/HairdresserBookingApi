a206ea1ced11a8bd38708789da2ec4086e7f5265
NeverGiveUPek <pdruczkin@gmail.com>
2022-04-03 11:55:26 +0200

    add all tests to WorkerControllerTests

--- HairdresserBookingApi.IntegrationTests/WorkerControllerTests.cs
+++ HairdresserBookingApi.IntegrationTests/WorkerControllerTests.cs
@@ -2,9 +2,12 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
+using HairdresserBookingApi.IntegrationTests.Helpers;
 using HairdresserBookingApi.Models.Db;
+using HairdresserBookingApi.Models.Dto.Worker;
 using HairdresserBookingApi.Models.Entities.Api;
 using Microsoft.Extensions.DependencyInjection;
+using NLog.Targets.Wrappers;
 using Xunit;
 
 namespace HairdresserBookingApi.IntegrationTests;
@@ -47,7 +50,7 @@ public class WorkerControllerTests : IClassFixture<CustomWebApplicationFactory<P
             FirstName = "Test",
             LastName = "Test",
             Email = "test@test.com",
-            PhoneNumber = "123123123"
+            PhoneNumber = "111222333"
         };
 
         SeedWorker(model);
@@ -65,6 +68,135 @@ public class WorkerControllerTests : IClassFixture<CustomWebApplicationFactory<P
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 
-    
+    [Fact]
+    public async Task Create_ForValidModel_ReturnsCreated()
+    {
+        var model = new Worker()
+        {
+            FirstName = "Test2",
+            LastName = "Test2",
+            Email = "test2@test.com",
+            PhoneNumber = "123123123"
+        };
+
+        var httpContent = model.ToJsonHttpContent();
+
+
+        var response = await _client.PostAsync("api/worker", httpContent);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+
+    [Fact]
+    public async Task Create_ForInValidModel_ReturnsBadRequest()
+    {
+        var model = new Worker()
+        {
+            FirstName = "Test",
+            Email = "WrongEmail"
+        };
+
+        var httpContent = model.ToJsonHttpContent();
+
+
+        var response = await _client.PostAsync("api/worker", httpContent);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Update_ForValidModelAndExistingWorker_ReturnsCreated()
+    {
+        var workerToUpdate = new Worker()
+        {
+            FirstName = "Test3",
+            LastName = "Test3",
+            Email = "test3@test.com",
+            PhoneNumber = "123456712"
+        };
+
+        SeedWorker(workerToUpdate);
+
+        var updateDto = new UpdateWorkerDto()
+        {
+            FirstName = "Test4",
+            LastName = "Test4",
+            Email = "test4@test.com",
+            PhoneNumber = "111222344"
+        };
+
+        var httpContent = updateDto.ToJsonHttpContent();
+
+        var response = await _client.PutAsync($"api/worker/{workerToUpdate.Id}", httpContent);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    }
+
+    [Fact]
+    public async Task Update_ForValidModelAndNonExistingWorker_ReturnsNotFound()
+    {
+        var updateDto = new UpdateWorkerDto()
+        {
+            FirstName = "Test5",
+            LastName = "Test5",
+            Email = "test5@test.com",
+            PhoneNumber = "111222355"
+        };
+
+        var httpContent = updateDto.ToJsonHttpContent();
+
+        var response = await _client.PutAsync($"api/worker/{-1}", httpContent);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task Update_ForInValidModel_ReturnsBadRequest()
+    {
+        //no need to seed, cause validation is before finding worker entity
+
+
+        //same email and phoneNumber, lack of LastName
+        var updateDto = new UpdateWorkerDto()
+        {
+            FirstName = "Test",
+            Email = "test@test.com",
+            PhoneNumber = "123123123"
+        };
+
+        var httpContent = updateDto.ToJsonHttpContent();
+
+        var response = await _client.PutAsync($"api/worker/{-1}", httpContent);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Delete_ForExistingWorker_ReturnsNoContent()
+    {
+        var workerToDelete = new Worker()
+        {
+            FirstName = "TestToDelete",
+            LastName = "TestToDelete",
+            Email = "testToDelete@test.com",
+            PhoneNumber = "888777444"
+        };
+
+        SeedWorker(workerToDelete);
+
+        var response = await _client.DeleteAsync($"api/worker/{workerToDelete.Id}");
+
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task Delete_ForNonExistingWorker_ReturnsNotFound()
+    {
+        var response = await _client.DeleteAsync($"api/worker/{-1}");
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
 
 }
